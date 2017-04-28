73624a090863066d28eac67a37d301710e8e8338
limmess <limmes@yahoo.com>
2017-04-28 16:02:27 +0300

    Add project files.

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace TheMaximumSubarray
+{
+    public class MaximumSubArrayDP
+    {
+        public static void Main(String[] args)
+        {
+            int[] arr = new int[] { 10, 14, 52, -2, 10, -22, -40, -3, 11 };
+            var xs =
+                    from n in System.Linq.Enumerable.Range(0, arr.Length)
+                    from l in System.Linq.Enumerable.Range(1, arr.Length - n)
+                    let subseq = arr.Skip(n).Take(l)
+                    orderby subseq.Count() descending
+                    orderby subseq.Sum() descending
+                    select subseq;
+            var maxSumSubseq = xs.First().Sum();
+            Console.WriteLine(maxSumSubseq);
+            Console.ReadKey();
+        }
+    }
+}
